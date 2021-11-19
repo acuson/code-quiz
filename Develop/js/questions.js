9596b6765280d662a664ca0ad5d5babb6d1622d1
Alyssa Cuson <acuson98@gmail.com>
2021-11-18 21:36:20 -0500

    finished adding functions to turn to next page after button click

--- Develop/js/questions.js
+++ Develop/js/questions.js
@@ -21,11 +21,11 @@ document.getElementById("start-btn").addEventListener("click", function show() {
     pages[i].style.display = "none";
 two.style.display = "block"}});
 
-//Clicking any button from page 2 to move to page 3
+//Variables for addQuestionListener function
 
 var question1 = document.getElementsByClassName("question-1");
 // var pageNumber3 = three.style.display;
-addQuestionListener(question1,three);
+addQuestionListener(question1, three);
 
 //Clicking any button from page 3 to move to page 4
 var question2 = document.getElementsByClassName("question-2");
@@ -45,6 +45,8 @@ addQuestionListener(question5, seven);
 // var allDone = document.getElementsByClassName("submit");
 // addQuestionListener(submit, eight);
 
+//Function that displays pages based on classes
+
 function addQuestionListener(question2, pageElement) {
   for (let i = 0; i < 4; i++) {
     let question = question2[i];
@@ -54,4 +56,14 @@ function addQuestionListener(question2, pageElement) {
       pages[i].style.display = "none";
       pageElement.style.display = "block"}});
     }
+}
+
+// User redirects to high score page after submitting form
+
+document.getElementById("form").onsubmit = function () {submitForm()}
+
+function submitForm (){
+    for (let i = 0; i < pages.length; i++) {
+      pages[i].style.display = "none";
+      eight.style.display = "block"};
 }
